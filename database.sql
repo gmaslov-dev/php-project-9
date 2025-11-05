73858be9c47d4c7d2926a69e6fe96ce2af611dff
gmaslov-dev <gennadiy.dev@yandex.ru>
2025-11-05 15:57:02 +0300

    fix composer

--- database.sql
+++ database.sql
@@ -1,7 +1,3 @@
-
--- DROP TABLE IF EXISTS checks;
--- DROP TABLE IF EXISTS urls;
-
 CREATE TABLE IF NOT EXISTS urls
 (
     id SERIAL PRIMARY KEY,
@@ -9,11 +5,6 @@ CREATE TABLE IF NOT EXISTS urls
     created_at TIMESTAMP NOT NULL
 );
 
-INSERT INTO urls (name, created_at)
-VALUES ('https://www.google.com', '2024-01-01 00:00:00'),
-    ('https://www.youtube.com', '2024-01-02 00:00:00');
-
--- TODO добавить таблицу checks
 CREATE TABLE IF NOT EXISTS checks
 (
     id SERIAL PRIMARY KEY,
